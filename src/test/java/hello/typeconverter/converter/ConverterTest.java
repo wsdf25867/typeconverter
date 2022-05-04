534d6394990c6b5397369e7c32da85e1a6f4d4f3
Ryan <wsdf25867@gamail.com>
2022-05-04 23:57:43 +0900

    MVC 2 스프링 타입 컨버터 - 타입 컨버터 - Converter 8:37

---
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,25 @@
+package hello.typeconverter.converter;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class ConverterTest {
+
+    @Test
+    void StringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    void IntegerToString() {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(10);
+        assertThat(result).isEqualTo("10");
+    }
+
+}
