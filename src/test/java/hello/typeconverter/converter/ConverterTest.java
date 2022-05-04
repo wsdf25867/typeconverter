d9791ed4b9ef713488e90e1a81f34ffa9515f7f0
Ryan <wsdf25867@gamail.com>
2022-05-05 00:15:45 +0900

    MVC 2 스프링 타입 컨버터 - 타입 컨버터 - Converter 끝

--- src/test/java/hello/typeconverter/converter/ConverterTest.java
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -1,6 +1,7 @@
 package hello.typeconverter.converter;
 
 
+import hello.typeconverter.type.IpPort;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -22,4 +23,18 @@ class ConverterTest {
         assertThat(result).isEqualTo("10");
     }
 
+    @Test
+    void StringToIpPort() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1",8080);
+        String result = converter.convert(source);
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void IpPortToString() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        IpPort result = converter.convert("127.0.0.1:8080");
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
 }
