58c023b4b600ef4a6a541a91294117ce6e3289f5
RyanKang <wsdf25867@gmail.com>
2022-05-06 12:46:21 +0900

    MVC 2 스프링 타입 컨버터 - 끝

--- src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
+++ src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
@@ -27,7 +27,6 @@ public class ConversionServiceTest {
 
         String ipPortString = conversionService.convert(new IpPort("127.0.0.1", 8080), String.class);
         assertThat(ipPortString).isEqualTo("127.0.0.1:8080");
-
-
     }
+
 }
